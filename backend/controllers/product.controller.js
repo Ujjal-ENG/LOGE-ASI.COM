e27e4a6561ab23aaa6558739d236f7e68071da2a
user <ujjalroy7862@gmail.com>
2022-12-02 15:13:48 +0600

    done another part of my project in backend that is filter the serach products based on user want

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -3,6 +3,8 @@ const Product = require("../models/product.model.js");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors.js");
 
+const APIFeatures = require("../utils/apiFeatures.js");
+
 // Create new product => /api/v1/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -12,9 +14,13 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-// Get all products => /api/v1/products
+// Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     count: products.length,
